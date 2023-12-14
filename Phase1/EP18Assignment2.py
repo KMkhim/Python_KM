1f256859ac7445aaef49cbe3772867e91c85af74
Korbun_S <cosecggirl@gmail.com>
2023-12-14 20:52:27 +0700

    EP1819

---
+++ Phase1/EP18Assignment2.py
@@ -0,0 +1,11 @@
+#compare morethan / lessthan
+
+num1 = int(input("num1 : "))
+num2 = int(input("num2 : "))
+
+if(num1 == num2) :
+    print("num1 is equal to num2 ")
+elif (num1 < num2) :
+    print("num2 is morethan num1")
+else :
+    print("num1 is morethan num2")
---
+++ Phase1/EP19Assignment3.py
@@ -0,0 +1,8 @@
+#odd end even
+
+a = int(input("input num : "))
+
+if (a % 2 == 0) :
+    print("It's even")
+else :
+    print("It's odd")
